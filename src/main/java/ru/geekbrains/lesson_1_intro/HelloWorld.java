a24b025c1aa6ce7acb9497d3985d9a69fae3ce68
dimkaNiko <83166315+dimkaNiko@users.noreply.github.com>
2021-04-25 16:42:35 +0500

    Update HelloWorld.java

--- src/main/java/ru/geekbrains/lesson_1_intro/HelloWorld.java
+++ src/main/java/ru/geekbrains/lesson_1_intro/HelloWorld.java
@@ -3,7 +3,8 @@ package ru.geekbrains.lesson_1_intro;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
-
+        
+        // ЗАДАНИЕ 2
         byte a = 20;
         short b = 21;
         int c = -15;
@@ -12,13 +13,20 @@ public class HelloWorld {
         double f = 120.26262262;
         char g = '*';
         boolean z = true;
-
+        
+        // ЗАДАНИЕ 3
         System.out.println("Это было просто: " + (calc(10.23f, 27.15f, 36f, 5.041f)));
+        // ЗАДАНИЕ 4
         System.out.println("С этими числами будет: " + (isTrueOrNot(15, 20)) + "!");
+        // ЗАДАНИЕ 5
         negPos(-21);
+        // ЗАДАНИЕ 6
         System.out.println("Как бы там ни было но: " + (posNeg(-10)) + "!");
+        // ЗАДАНИЕ 7
         nameOf("Деметрос");
+        // ЗАДАНИЕ 8 
         leapYear(2020);
+        
     }
             static float calc(float a, float b, float c, float d){
                 return a * (b + (c / d));
